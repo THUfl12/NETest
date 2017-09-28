752f0be34e16d804889aeb691bb3f6bf7db5d5e2
THUfl12 <1027555983@qq.com>
2017-09-28 22:15:58 +0800

    modify test_py and node2vec

--- gem/node2vec/main.py
+++ gem/node2vec/main.py
@@ -17,55 +17,56 @@ from gensim.models import Word2Vec
 from six import iteritems
 
 
-def parse_args():
+def parse_args(file_path, output_path, dimension=2, wlength=80, nwalks=10, wsize=10, epochs=2,
+			   p=1, q=1, weighted=False, directed=False):
 	'''
 	Parses the node2vec arguments.
 	'''
 	parser = argparse.ArgumentParser(description="Run node2vec.")
 
-	parser.add_argument('--input', nargs='?', default='../data/karate.edgelist',
+	parser.add_argument('--input', nargs='?', default=file_path,
 	                    help='Input graph path')
 
-	parser.add_argument('--output', nargs='?', default='karate.emb',
+	parser.add_argument('--output', nargs='?', default=output_path,
 	                    help='Embeddings path')
 
-	parser.add_argument('--dimensions', type=int, default=2,
+	parser.add_argument('--dimensions', type=int, default=dimension,
 	                    help='Number of dimensions. Default is 2.')
 
-	parser.add_argument('--walk-length', type=int, default=80,
+	parser.add_argument('--walk-length', type=int, default=wlength,
 	                    help='Length of walk per source. Default is 80.')
 
-	parser.add_argument('--num-walks', type=int, default=10,
+	parser.add_argument('--num-walks', type=int, default=nwalks,
 	                    help='Number of walks per source. Default is 10.')
 
-	parser.add_argument('--window-size', type=int, default=10,
+	parser.add_argument('--window-size', type=int, default=wsize,
                     	help='Context size for optimization. Default is 10.')
 
-	parser.add_argument('--iter', default=2, type=int,
+	parser.add_argument('--iter', default=epochs, type=int,
                       help='Number of epochs in SGD')
 
 	parser.add_argument('--workers', type=int, default=8,
 	                    help='Number of parallel workers. Default is 8.')
 
-	parser.add_argument('--p', type=float, default=1,
+	parser.add_argument('--p', type=float, default=p,
 	                    help='Return hyperparameter. Default is 1.')
 
-	parser.add_argument('--q', type=float, default=1,
+	parser.add_argument('--q', type=float, default=q,
 	                    help='Inout hyperparameter. Default is 1.')
 
 	parser.add_argument('--weighted', dest='weighted', action='store_true',
 	                    help='Boolean specifying (un)weighted. Default is unweighted.')
 	parser.add_argument('--unweighted', dest='unweighted', action='store_false')
-	parser.set_defaults(weighted=False)
+	parser.set_defaults(weighted=weighted)
 
 	parser.add_argument('--directed', dest='directed', action='store_true',
 	                    help='Graph is (un)directed. Default is undirected.')
 	parser.add_argument('--undirected', dest='undirected', action='store_false')
-	parser.set_defaults(directed=False)
+	parser.set_defaults(directed=directed)
 
 	return parser.parse_args()
 
-def read_graph():
+def read_graph(args):
 	'''
 	Reads the input network in networkx.
 	'''
@@ -81,7 +82,7 @@ def read_graph():
 
 	return G
 
-def learn_embeddings(walks):
+def learn_embeddings(args, walks):
 	'''
 	Learn embeddings by optimizing the Skipgram objective using SGD.
 	'''
@@ -89,9 +90,9 @@ def learn_embeddings(walks):
 	model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers, iter=args.iter)
 	model.wv.save_word2vec_format(args.output)
 
-	# for word, vocab in sorted(iteritems(model.wv.vocab), key=lambda item: -item[1].count):
-	# 	print(word, vocab.index)
-	# 	print(word, model.wv.syn0[vocab.index])
+	for word, vocab in sorted(iteritems(model.wv.vocab), key=lambda item: -item[1].count):
+		print(word, vocab.index)
+		print(word, model.wv.syn0[vocab.index])
 
 	return
 
@@ -99,11 +100,11 @@ def main(args):
 	'''
 	Pipeline for representational learning for all nodes in a graph.
 	'''
-	nx_G = read_graph()
+	nx_G = read_graph(args)
 	G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
 	G.preprocess_transition_probs()
 	walks = G.simulate_walks(args.num_walks, args.walk_length)
-	learn_embeddings(walks)
+	learn_embeddings(args, walks)
 
 if __name__ == "__main__":
 	args = parse_args()
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from gem.embedding.gf import GraphFactorization as gf
-from gem.embedding import node2vec as nv
+from gem.embedding.lle import LocallyLinearEmbedding as lle
+from gem.embedding.hope import HOPE
+from gem.embedding.lap import LaplacianEigenmaps as lap
+
+from gem.node2vec.main import parse_args as input_args
+from gem.node2vec.main import main as nv
+
 from gem.evaluation import evaluate_graph_reconstruction as gr
 from gem.utils import graph_util
 from gem.evaluation import visualize_embedding as viz
@@ -8,21 +14,33 @@ import matplotlib.pyplot as plt
 from subprocess import call
 
 
-# em = gf(2, 1000, 1*10**-4, 1.0)
-# graph = graph_util.loadGraphFromEdgeListTxt('data/karate.edgelist', directed=False)
-#
-# Y, t = em.learn_embedding(graph, edge_f=None, is_weighted=True, no_python=True)
-# MAP, prec_curv = gr.evaluateStaticGraphReconstruction(graph, em, Y, None)
-
 edge_f = 'data/karate.edgelist'
 G = graph_util.loadGraphFromEdgeListTxt(edge_f, directed=False)
-G = G.to_directed()
 res_pre = 'results/testKarate'
 print 'Num nodes: %d, num edges: %d' % (G.number_of_nodes(), G.number_of_edges())
-t1 = time()
-embedding = nv.node2vec(2, 1, 80, 10, 10, 1, 1)
-embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
-print 'node2vec:\n\tTraining time: %f' % (time() - t1)
 
-viz.plot_embedding2D(embedding.get_embedding(), di_graph=G, node_colors=None)
-plt.show()
+# graph factorization
+embedding = gf(2, 100, 1*10**-4, 1.0)   # d, max_iter, eta, regu
+Y, t = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
+print 'Graph Factorization:\n\tTraining time: %f' % t
+
+# Locally Linear Embedding
+embedding = lle(2)  # d
+Y, t = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
+print 'Locally Linear Embedding:\n\tTraining time: %f' % t
+
+# Hope
+embedding = HOPE(4, 0.01)   # d, beta
+Y, t = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
+print 'HOPE:\n\tTraining time: %f' % t
+
+# Laplacian Eigen maps
+embedding = lap(4)   # d
+Y, t = embedding.learn_embedding(graph=G, edge_f=None, is_weighted=True, no_python=True)
+print 'Laplacian Eigenmaps:\n\tTraining time: %f' % t
+
+# node2vec
+args = input_args(file_path='data/karate.edgelist', output_path='data/karate.emb')
+nv(args)
+
+
